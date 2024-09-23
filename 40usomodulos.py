fa42fd93cfb6474775a2715a63fd0f25767c5952
Freddy Mercado <frmercado@emcali.com.co>
2024-09-23 14:12:01 -0500

    230924-1411

---
+++ 40usomodulos.py
@@ -0,0 +1,8 @@
+from calculos.calculos_generales import * #Se pone primero la carpeta y luego el programa, cuanod se esta en otra carpeta el modulo a llamar
+
+
+dividir(123,456)
+sumar(123,345)
+redondear(123.456)
+potencia(2,3)
+
---
+++ calculos/calculos_generales.py
@@ -0,0 +1,21 @@
+def sumar(op1, op2):
+    print("la suma es : ", op1+op2)
+
+def restar(op1, op2):
+    print("la resta es : ", op1-op2)
+
+def multiplicar(op1, op2):
+    print("la  multiplicacion es : ", op1*op2)
+
+def dividir(op1,op2):
+
+    print("la division es : ", op1/op2)
+
+def potencia(base,exponente):
+
+    print("El resultado es : ", base**exponente)
+
+def redondear(numero):
+    print("El resultado es :", round(numero))
+
+    
