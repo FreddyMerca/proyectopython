11fbdb01e4bf1dd11314eaf9ad0994df80ce051d
Freddy Mercado <frmercado@emcali.com.co>
2024-09-29 21:19:54 -0500

    290924-2119

--- decoradoresII.py
+++ decoradoresII.py
@@ -2,31 +2,33 @@
 
 def funcion_decoradora(funcion_parametro):
 
-    def funcion_interior():
+    def funcion_interior(*args, **kwargs):
 
         # Acciones adiconales que decoran 
 
         print("Vamos a realizar un calculo")
 
-        funcion_parametro()
+        funcion_parametro(*args, **kwargs)
 
         print("Hemos terminado el calculo")
 
     return funcion_interior
 
 @funcion_decoradora #Aqui se llama a la funcion decoradora y hata lo que esta dentro de dicha funcion al hacer la funcion suma
-def suma(): 
+def suma(num1, num2, num3): 
 
-    print(15+20)
+    print(num1+num2+num3)
 
 @funcion_decoradora
-def resta():
+def resta(num1, num2):
 
-    print(30-10)
+    print(num1-num2)
+    
+@funcion_decoradora
+def potencia(base, exponente):
+    
+    print(pow(base, exponente))
 
-
-suma()
-resta()
-
-
-
+suma(7,5,8)
+resta(12,10)
+potencia(base=5,exponente=3)
